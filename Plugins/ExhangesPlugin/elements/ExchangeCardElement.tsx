4b37ea368d87c50a67ff659fdcb7712636c3e5e0
guga-ggw <ggwomega15@gmail.com>
2025-03-15 23:19:03 +0400

    add new design of exchange project card

--- Plugins/ExhangesPlugin/elements/ExchangeCardElement.tsx
+++ Plugins/ExhangesPlugin/elements/ExchangeCardElement.tsx
@@ -1,28 +1,14 @@
-import React, { ReactElement, ReactNode, useState } from "react";
-import {
-  CalendarDays,
-  DollarSign,
-  Globe,
-  User,
-  ChevronDown,
-  MapIcon,
-  X,
-} from "lucide-react";
+import React, { useState } from "react";
+import { ChevronDown, X } from "lucide-react";
 import { motion } from "framer-motion";
-
-type ButtonProps = {
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
-};
+import { BsClipboardData } from "react-icons/bs";
+import { FaUserEdit } from "react-icons/fa";
+import { MdOutlineFlight } from "react-icons/md";
+import { SiSololearn } from "react-icons/si";
 
 type CardProps = {
   children: React.ReactNode;
   className?: string;
-};
-
-type CardContentProps = {
-  children: React.ReactNode;
 };
 
 type AccordionProps = {
@@ -30,39 +16,19 @@ type AccordionProps = {
   children: React.ReactNode;
 };
 
-type InfoItemProps = {
-  icon: ReactNode;
-  label: string;
-  value: string | number;
-  iconColor: string;
-};
-
-const Button = ({ children, className, onClick }: ButtonProps) => (
-  <button
-    onClick={onClick}
-    className={`px-6 py-3 rounded-xl transition duration-300 ease-in-out ${className}`}
-  >
-    {children}
-  </button>
-);
-
 const Card = ({ children, className }: CardProps) => (
   <div
-    className={`bg-white rounded-2xl p-1 md:p-3 lg:p-6 py-4 sm:p-8 ${className}`}
+    className={`bg-white rounded-2xl flex flex-col gap-11 w-full p-1 md:p-3 lg:p-6 py-4 sm:p-8 ${className}`}
   >
     {children}
   </div>
-);
-
-const CardContent = ({ children }: CardContentProps) => (
-  <div className="space-y-6">{children}</div>
 );
 
 const Accordion = ({ title, children }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="border border-gray-200 p-4 rounded-xl">
+    <div className="border bg-[#DEE6ED] p-4 rounded-xl">
       <button
         className="flex justify-between items-center w-full text-left font-semibold text-gray-800 cursor-pointer"
         onClick={() => setIsOpen(!isOpen)}
@@ -79,152 +45,75 @@ const Accordion = ({ title, children }: AccordionProps) => {
   );
 };
 
-const InfoItem = ({ icon, label, value, iconColor }: InfoItemProps) => {
-  const renderIcon = () => {
-    if (React.isValidElement(icon)) {
-      return React.cloneElement(icon as ReactElement);
-    } else {
-      return <span className={`w-5 h-5 ${iconColor}`}>{icon}</span>;
-    }
-  };
-
-  return (
-    <div className={`${iconColor} flex gap-2 items-center`}>
-      {renderIcon()}
-      <span className="text-gray-700">
-        {label}:{" "}
-        {label == "გამშვები" ? (
-          <a href="Organisations/123" className="underline">
-            <strong>{value}</strong>
-          </a>
-        ) : (
-          <strong>{value}</strong>
-        )}
-      </span>
-    </div>
-  );
-};
-const SectionDivider = () => (
-  <div className="border-t border-gray-200 my-6"></div>
-);
-
 export default function ScholarshipCard({ onClose }: { onClose: () => void }) {
   return (
     <motion.div
       initial={{ opacity: 0, x: -20 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex justify-start items-center w-full md:w-[50%] left-0 absolute top-0 flex-col h-screen xl:w-[32%] bg-white p-1 md:p-2 lg:p-4 overflow-y-auto max-h-screen hide-scrollbar"
+      className="flex justify-start items-center w-full md:w-[50%] left-0 absolute top-0 flex-col h-screen xl:w-[32%] bg-white p-1 md:p-2 lg:p-4 xl:pt-8 overflow-y-auto max-h-screen hide-scrollbar"
     >
       <X
         className="absolute top-2 right-2 cursor-pointer"
         onClick={() => onClose()}
       />
-      <h2 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-2 text-center px-5">
+      <h2 className="text-2xl sm:text-4xl font-bold text-gray-900 mb-2 text-left px-2">
         გაცვლითი პროგრამა თბილისში
       </h2>
       <Card>
-        <CardContent>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:flex md:justify-between">
-            <InfoItem
-              icon={<User />}
-              label="ასაკი"
-              value="18+"
-              iconColor="text-blue-600"
-            />
-            <InfoItem
-              icon={<MapIcon />}
-              label="ლოკაცია"
-              value="ირლანდია, კორკი"
-              iconColor="text-red-500"
-            />
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex gap-4 justify-start text-[#416E9A] items-center w-full">
+            <BsClipboardData className="text-2xl" />
+            <h1>პერსონალური ინფორმაცია</h1>
           </div>
-          <SectionDivider />
-
-          <InfoItem
-            icon={<Globe />}
-            label="გამშვები"
-            value="Erasmus+"
-            iconColor="text-green-600"
-          />
-          <InfoItem
-            icon={<Globe />}
-            label="მასპინძელი"
-            value="University of Edinburgh"
-            iconColor="text-purple-600"
-          />
-          <SectionDivider />
-
-          <InfoItem
-            icon={<DollarSign />}
-            label="დაფინანსება"
-            value="სრული"
-            iconColor="text-green-500"
-          />
-          <InfoItem
-            icon={<DollarSign />}
-            label="საფასური"
-            value="60$"
-            iconColor="text-green-600"
-          />
-          <SectionDivider />
-
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-            <InfoItem
-              icon={<CalendarDays />}
-              label="გამგზავრება"
-              value="04.05.2025"
-              iconColor="text-gray-500"
-            />
-            <InfoItem
-              icon={<CalendarDays />}
-              label="ჩამოსვლა"
-              value="10.05.2025"
-              iconColor="text-gray-500"
-            />
-            <InfoItem
-              icon={<CalendarDays />}
-              label="deadline"
-              value="25.05.2025"
-              iconColor="text-gray-500"
-            />
+          <div className="w-full border-l-[2px] border-[#7A98B5]  px-6 text-[#053359] text-lg flex flex-col gap-2">
+            ასაკი : 18+
           </div>
-          <SectionDivider />
-
-          <div className="space-y-4">
-            <Accordion title="კრიტერიუმები">
-              <ul className="list-disc list-inside space-y-2">
-                <li>უნდა იყოს მინიმუმ 18 წლის</li>
-                <li>უნდა ფლობდე ინგლისურ ენას</li>
-                <li>უნდა გქონდეს შესაბამისი განათლება ან გამოცდილება</li>
-              </ul>
-            </Accordion>
-            <Accordion title="რა არ ფინანსდება">
-              <ul className="list-disc list-inside space-y-2">
-                <li>სამოგზაურო დაზღვევა</li>
-                <li>ვიზის ხარჯები</li>
-                <li>პირადი ხარჯები</li>
-              </ul>
-            </Accordion>
-            <Accordion title="დაფინანსება მოიცავს">
-              <ul className="list-disc list-inside space-y-2">
-                <li>საცხოვრებელი</li>
-                <li>კვება</li>
-                <li>ტრანსპორტი</li>
-              </ul>
-            </Accordion>
-            <Accordion title="დამატებითი მასალა">
-              <p className="text-gray-700">
-                გთხოვთ, წარადგინოთ CV, სამოტივაციო წერილი და სარეკომენდაციო
-                წერილი.
-              </p>
-            </Accordion>
+        </div>
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex gap-4 justify-start text-[#416E9A] items-center w-full">
+            <SiSololearn className="text-2xl" />
+            <h1>ორგანიზაციები</h1>
           </div>
-
-          <Button className="w-full mt-6 bg-blue-600 hover:bg-blue-700 text-white font-semibold">
-            რეგისტრაციის ფორმა
-          </Button>
-        </CardContent>
+          <div className="w-full border-l-[2px] border-[#7A98B5]  px-6 text-[#053359] text-lg flex flex-col gap-3">
+            <h1>
+              გამგზავნი : <a href="">Erasmus +</a>
+            </h1>
+            <h1>
+              მიმღები : <a href="">Ados family</a>
+            </h1>
+          </div>
+        </div>
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex gap-4 justify-start text-[#416E9A] items-center w-full">
+            <MdOutlineFlight className="text-2xl" />
+            <h1>პროექტის დეტალები</h1>
+          </div>
+          <div className="w-full border-l-[2px] border-[#7A98B5]  px-6 text-[#053359] text-lg flex flex-col gap-2">
+            <h1>ქვეყანა : საქართველო</h1>
+            <h1>დაფინანსება : ნაწილობრივ</h1>
+            <h1>საფასური : 200$</h1>
+            <h1>გამგზავნების თარიღი : 10.05.2025</h1>
+            <h1>დაბრუნების თარიღი : 25.05.2025</h1>
+          </div>
+        </div>
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex gap-4 justify-start text-[#416E9A] items-center w-full">
+            <FaUserEdit className="text-2xl" />
+            <h1>რეგისტრაცია</h1>
+          </div>
+          <div className="w-full border-l-[2px] border-[#7A98B5]  px-6 text-[#053359] text-lg flex flex-col gap-2">
+            <h1>რეგისტრაციის დედლაინი : 10.04.2025</h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <Accordion title="მეტი ინფორმაცია">
+            <h1>აქ არის მეტი ინფორმაცია ...</h1>
+          </Accordion>
+          <button className="w-full rounded-xl bg-[#205781] text-white text-xl py-3">
+            სარეგისტრაციო ფორმა
+          </button>
+        </div>
       </Card>
     </motion.div>
   );
--- Plugins/OrganisationsPlugin/Components/OrganisationDetailPageComponent.tsx
+++ Plugins/OrganisationsPlugin/Components/OrganisationDetailPageComponent.tsx
@@ -17,15 +17,12 @@ const OrganisationDetailPage = () => {
 
   return (
     <div className="w-full min-h-screen ">
-      {/* Back Button & Text Section */}
       <div className="flex flex-col xl:flex-row items-center xl:items-start xl:gap-6 ">
         <div className="flex flex-col gap-11 w-full xl:w-2/5 md:px-12 px-1 py-2 md:py-16">
-          {/* Back Button */}
           <MdKeyboardArrowLeft
             className="text-white w-12 h-12 bg-gray-300 rounded-full p-3 cursor-pointer"
             onClick={() => navigate.push("/Exchanges")}
           />
-          {/* Title & Description */}
           <div className="flex flex-col gap-7">
             <h1 className="text-3xl md:text-5xl text-[#488285] font-bold">
               Youth Center of Georgia
@@ -43,15 +40,12 @@ const OrganisationDetailPage = () => {
               რაც ახალგაზრდებს გლობალური კავშირების დამყარებაში ეხმარება.
             </p>
           </div>
-          {/* Website Button */}
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-xl px-6 py-3 w-36">
             ვებსაიტი
           </button>
         </div>
 
-        {/* Image Section */}
         <div className="relative w-full xl:w-3/5 h-[65vh] xl:h-[70vh]">
-          {/* Background Image */}
           <Image
             src={image.src}
             alt="globe"
@@ -60,7 +54,6 @@ const OrganisationDetailPage = () => {
             className="w-full h-full object-cover"
           />
 
-          {/* Floating Cards */}
           <div className="absolute inset-0">
             <div className="absolute top-[20%] left-[5%] lg:left-[45%] xl:left-[2%] 2xl:left-[40%] bg-gray-200 rounded-xl shadow-lg w-40 h-56 md:w-52 md:h-72 flex items-center  xl:w-96 justify-center">
               📷
@@ -75,7 +68,6 @@ const OrganisationDetailPage = () => {
         </div>
       </div>
 
-      {/* Additional Content (Optional) */}
       <div className="w-full h-fit bg-[#d8f4f5]  p-12">
         <div className="w-full flex justify-between">
           <h1 className="text-[#37368A] text-4xl font-semibold">
